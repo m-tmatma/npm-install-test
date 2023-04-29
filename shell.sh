94a7845aea5703e0667926edf58d7e3256f73038
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-04-29 13:08:50 +0900

    replace && with ;

--- shell.sh
+++ shell.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -it npm-install-test bash -c 'export XDG_CONFIG_HOME=/work && export NVM_DIR="$XDG_CONFIG_HOME/nvm" && . "$NVM_DIR/nvm.sh"; bash'
+docker run -it npm-install-test bash -c 'export XDG_CONFIG_HOME=/work; export NVM_DIR="$XDG_CONFIG_HOME/nvm"; . "$NVM_DIR/nvm.sh"; bash'
